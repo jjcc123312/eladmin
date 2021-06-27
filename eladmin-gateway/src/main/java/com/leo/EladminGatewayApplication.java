71725fa5b00ee1669bac0df08081d7aeff3328e2
jjcc123312 <47164229+jjcc123312@users.noreply.github.com>
2021-06-27 23:43:56 +0800

    服务启动连接nacos完成

--- eladmin-gateway/src/main/java/com/leo/EladminGatewayApplication.java
+++ eladmin-gateway/src/main/java/com/leo/EladminGatewayApplication.java
@@ -2,16 +2,26 @@ package com.leo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * @author Jjcc
  */
 @SpringBootApplication
+//@ConfigurationProperties("name1.jjcc1")
 public class EladminGatewayApplication {
+
+    private String name;
 
     public static void main(String[] args) {
         SpringApplication.run(EladminGatewayApplication.class, args);
     }
 
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
