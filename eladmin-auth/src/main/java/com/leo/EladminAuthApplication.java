cfb5ffa8e90ba01cc885622bee0ec3d17b399b90
jjcc123312 <47164229+jjcc123312@users.noreply.github.com>
2021-06-29 20:08:27 +0800

    ...

---
+++ eladmin-auth/src/main/java/com/leo/EladminAuthApplication.java
@@ -0,0 +1,13 @@
+package com.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EladminAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EladminAuthApplication.class, args);
+    }
+
+}
